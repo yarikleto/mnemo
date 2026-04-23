6edcb34ae67638932069d0e34223fecaefcf4735
Yaroslav Pakhaliuk <yaroslavp@wix.com>
2026-04-23 17:29:08 +0700

    feat(preload): expose typed api to renderer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,3 +1,33 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
+import type { Api } from '../shared/api'
 
-contextBridge.exposeInMainWorld('api', {})
+const invoke = <T>(ch: string, args?: unknown): Promise<T> => ipcRenderer.invoke(ch, args ?? null) as Promise<T>
+const on = (ch: string, cb: (...a: any[]) => void) => {
+  const handler = (_e: unknown, ...args: any[]) => cb(...args)
+  ipcRenderer.on(ch, handler)
+  return () => ipcRenderer.off(ch, handler)
+}
+
+const api: Api = {
+  listNamespaces: () => invoke('listNamespaces'),
+  listCards: (namespace) => invoke('listCards', namespace),
+  getDueQueue: (filter) => invoke('getDueQueue', filter),
+  readCard: (id) => invoke('readCard', id),
+  getDashboardData: (widgets) => invoke('getDashboardData', widgets),
+  createCard: (input) => invoke('createCard', input),
+  updateCard: (input) => invoke('updateCard', input),
+  moveCard: (input) => invoke('moveCard', input),
+  deleteCard: (id) => invoke('deleteCard', id),
+  rateReview: (input) => invoke('rateReview', input),
+  openInExternalEditor: (id) => invoke('openInExternalEditor', id),
+  getConfig: () => invoke('getConfig'),
+  updateConfig: (patch) => invoke('updateConfig', patch),
+  searchCards: (q) => invoke('searchCards', q),
+  rescan: () => invoke('rescan'),
+  onCardChanged: (cb) => on('card-changed', cb),
+  onCardAdded: (cb) => on('card-added', cb),
+  onCardRemoved: (cb) => on('card-removed', cb),
+  onIndexRebuilt: (cb) => on('index-rebuilt', cb)
+}
+
+contextBridge.exposeInMainWorld('api', api)
